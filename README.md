6236334dbef1a6d256673bc9d2cc1147c2b6d25c
teknoid <v@vpc.(none)>
2009-10-08 11:27:36 -0400

    Validate both models at once, a few bug fixes Changes to README

--- README.md
+++ README.md
@@ -1,39 +1,40 @@
-### Habtamable behavior (!!! Still alpha, quite a few to-do's !!!)
-
-Basic goals:
-Save two HABTM models at once.
-For example you are saving a Location (my sweet penthouse) with all the relevant info
-and the Address (the actual address of the penthouse).
-
-Allow to search accross HABTM models
-For example, you can do something like:
-$this->Location->find('all', array('conditions' => array('Location.is_active' => 1, 'Address.city' => 'Miami')));
-
-(The searching works by "faking" a hasOne bind, and therefore building a join to specify conditions
-for the time being only INNER join is supported and the bind is not permament)
-
-Of couse, Location HABTM Address
-
-Features (so far):
-
-- If address exists will use existing ID
-- Will ensure proper relationship saving
-- Model validation (needs work)
-- Allows you to search across HABTM models
-
-Very simple to use:
-
-Using our example above, in the Location model, all you need to do is to add 
-public $actsAs = array('Habtamable');
-
-The above presumes that in the Location model definition you have:
-public $hasAndBelongsToMany = array('Address');
-
-The rest is taken care of for you.
-
-Suggestions, improvments, etc. are needed.
-[Contact here](http://wp.me/peDIi-cZ)
-
-
-**Major to-do: Validate both models at once**
-
+> ### Habtamable behavior (!!! Still alpha, quite a few to-do's !!!)
+>
+> *Basic goals:*
+> 
+> 1. Save two HABTM models at once.
+> For example you are saving a Location (my sweet penthouse) with all the relevant info
+> and the Address (the actual address of the penthouse).
+>
+> Example: $this->Location->saveAll($this->data);
+> In the above example Location and Address will be saved with transactional support
+> save() works just as well.
+>
+> 2. Search accross HABTM models
+> For example, you can do something like:
+> $this->Location->find('all', array('conditions' => array('Location.is_active' => 1, 'Address.city' => 'Miami')));
+> 
+> (The searching works by "faking" a hasOne bind, and therefore building a join to specify conditions
+> for the time being only INNER join is supported and the bind is not permament)
+> 
+> Of couse, Location HABTM Address
+> 
+> Features (so far):
+> 
+> - If address (HABTM record) exists will use existing ID
+> - Will ensure proper relationship saving
+> - Model validation 
+> - Allows you to search across HABTM models
+> 
+> Very simple to use:
+> 
+> Using our example above, in the Location model, all you need to do is to add 
+> *public $actsAs = array('Habtamable');*
+> 
+> The above presumes that in the Location model definition you have:
+> *public $hasAndBelongsToMany = array('Address');*
+> 
+> The rest is taken care of for you.
+> 
+> Suggestions, improvments, etc. are needed.
+> [Contact here](http://wp.me/peDIi-cZ)
--- models/behaviors/habtamable.php
+++ models/behaviors/habtamable.php
@@ -3,6 +3,8 @@
  * Save any two HABTM related models at the same time... with existing record check
  * i.e. Loaction HABTM Address, will save both at once. (if a given address does not exist 
  * in the addresses table, it will save address and relation, otherwise we'll get existing Address.id and save relation)
+ * Use saveAll() to ensure transactional support. Remember that your DB should support transactions.
+ * If you don't care (?) about transactions, save() works just as well.
  * 
  * Search accross HABTM Models, without modifying your find:
  * $this->Location->find('all', array('conditions' => array('Address.city' => 'Miami')));
@@ -11,29 +13,24 @@
    
  /**
  * No need to check these fields, as they are pretty much always unique
+ * These will really depend on your app, I've included some common ones for mine
  */
-   private $fieldsToSkip = array('id', 'created', 'modified', 'updated');
+   private $fieldsToSkip = array('id', 'created', 'modified', 'updated', 'lat', 'lng', 'is_active', 'pid');
    
  /**
  * Related model. Using example above, it would be Address
  */   
-   private $habtmModel;
-
- /**
- * Hold conditions being passed to the find()
- */  
-  private $conditions;
-  
+   private $habtmModel;  
  /**
  * Figure out what models we are working with.
- * ... and set relevant class properties.
+ * ... and set relevant properties.
  */    
    public function setup(&$Model, $settings = array()) { 
     if(empty($settings)) {    
       $this->settings[$Model->alias]['habtmModel'] = key($Model->hasAndBelongsToMany);
     }
     
-    $this->habtmModel = $this->settings[$Model->alias]['habtmModel'];   
+    $this->habtmModel = $this->settings[$Model->alias]['habtmModel'];  
   }
   
 /**
@@ -44,11 +41,10 @@
     
     return TRUE;  
   }
- 
+
 /**  
- * Fake model bindings and construct a join
- */
- 
+ * Fake model bindings and construct a JOIN
+ */ 
   private function changeBind(&$Model) {
         
     $Model->bindModel(array('hasOne' => array($Model->hasAndBelongsToMany[$this->habtmModel]['with'] => array(
@@ -64,20 +60,46 @@
                                                   $this->habtmModel . '.' . $Model->{$this->habtmModel}->primaryKey . ' = ' . $Model->hasAndBelongsToMany[$this->habtmModel]['with'] . '.' .
                                                   $Model->hasAndBelongsToMany[$this->habtmModel]['foreignKey']                                                  
                                                 )))));  
-  }  
-  /**
+  }
+  
+/**
+ * A little hack to simulate validation of both models at once
+ * (possibly needs re-thinking)
+ */  
+  public function beforeValidate(&$Model) {
+    if(!$this->doValidation($Model)) {
+      $Model->invalidate(NULL);
+    }
+  }
+ 
+ /**
+  * If related HABTM model fails validation we invalidate
+  * our other
+  */ 
+  private function doValidation(&$Model) {
+    $Model->{$this->habtmModel}->set($Model->data[$this->habtmModel]);
+    
+    if($Model->{$this->habtmModel}->validates()) {
+      return TRUE;
+    }
+    
+    return FALSE;
+  }
+  
+ /**
  * If we have an existing record matching our input data, then all we need is the record ID.
  */    
-   public function beforeSave(&$Model) {
-                       
-      $existingId = $this->getExistingId($Model);
-      
-      if(!empty($existingId)) {
-        $Model->data[$this->habtmModel][$this->habtmModel] = $existingId;
-      }
+   public function beforeSave(&$Model) {       
+    $existingId = $this->getExistingId($Model);
+        
+    if(!empty($existingId)) {
+      $Model->data[$this->habtmModel][$this->habtmModel] = $existingId;
+    }
             
     return TRUE;
   }
+  
+  
   
  /**
  * Either save record and get new ID.
@@ -104,8 +126,9 @@
  /**
  * Building proper conditions to search our HABTM model for an existing record
  */  
-  private function buildCondition(&$Model) {    
-    foreach($Model->{$this->habtmModel}->schema() as $field => $type) {      
+  private function buildCondition(&$Model) { 
+     
+    foreach($Model->{$this->habtmModel}->schema() as $field => $meta) {
       if(!in_array($field, $this->fieldsToSkip)) {
         if(isset($Model->data[$this->habtmModel][$field]) && !empty($Model->data[$this->habtmModel][$field])) {                   
           $conditions[] = array($Model->{$this->habtmModel}->alias . '.' . $field => $Model->data[$this->habtmModel][$field]);  
